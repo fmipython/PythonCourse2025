e192540031c7eaeaf7eb2b5b439fee1114ff01a4
Aleksandar <yalishanda@abv.bg>
2025-10-18 17:53:53 +0300

    Fix text input

--- workshop1/cipher.py
+++ workshop1/cipher.py
@@ -3,7 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("mode", choices=["encrypt", "decrypt"], help="Mode of operation (encrypt or decrypt)")
-parser.add_argument("text", help="Text to be processed")
+parser.add_argument("text", nargs="?", help="Text to be processed")
 parser.add_argument("-k", "--key", type=int, required=True, help="Key for encryption/decryption")
 parser.add_argument("-a", "--alphabet", default="en", help="Alphabet to use")
 args = parser.parse_args()
